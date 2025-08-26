92cbe53e35938d36772de52426269eba629c5147
hamtana156@gmail.com <phiha428@student.otago.ac.nz>
2025-08-26 13:58:52 +1200

    added nta springboot files

---
+++ NTA/src/test/java/com/CCDHB/NTA/NtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CCDHB.NTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
